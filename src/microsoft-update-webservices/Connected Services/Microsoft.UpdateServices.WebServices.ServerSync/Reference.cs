0476a48f3894bf2ee3f0d332dc6b61e0de20a4f3
Antonio Santana <ansantan@microsoft.com>
2025-10-24 11:52:28 -0700

    * 'Line 13: Added the missing using directive for System.ServiceModel to resolve the missing namespace error for OperationContractAttribute.' in file 'src\microsoft-update-webservices\Connected Services\Microsoft.UpdateServices.WebServices.ServerSync\Reference.cs'

--- src/microsoft-update-webservices/Connected Services/Microsoft.UpdateServices.WebServices.ServerSync/Reference.cs
+++ src/microsoft-update-webservices/Connected Services/Microsoft.UpdateServices.WebServices.ServerSync/Reference.cs
@@ -11,6 +11,7 @@ namespace Microsoft.UpdateServices.WebServices.ServerSync
 {
 #pragma warning disable 1591
     using System.Runtime.Serialization;
+    using System.ServiceModel;
 
     [System.Diagnostics.DebuggerStepThroughAttribute()]
     [System.CodeDom.Compiler.GeneratedCodeAttribute("dotnet-svcutil", "1.0.0.1")]
